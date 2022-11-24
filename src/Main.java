f1d80c884fb156452d9b27e19b3e030cf6dc198d
BogatyrIrina <kochereva_irina@mail.ru>
2022-11-24 20:13:42 +0300

    Задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        Author author1 = new Author("Федор", "Достоевский");
+        System.out.println("author1.getName() = " + author1.getName());
+        System.out.println("author1.getSurname() = " + author1.getSurname());
+
+        Book book1 = new Book("Преступление и наказание", author1, 1995);
+        System.out.println("book1.getName() = " + book1.getName());
+        System.out.println("book1.getPublicationDate() = " + book1.getPublicationDate());
+
+        book1.setPublicationDate(1992);
+        System.out.println("book1.getPublicationDate() = " + book1.getPublicationDate());
+
+        Author author2 = new Author("Шарлотта", "Бронте");
+        System.out.println("author2.getName() = " + author2.getName());
+        System.out.println("author2.getSurname() = " + author2.getSurname());
+
+        Book book2 = new Book("Джэйн Эйр", author2, 1990);
+        System.out.println("book2.getName() = " + book2.getName());
+        System.out.println("book2.getPublicationDate() = " + book2.getPublicationDate());
+
+        book2.setPublicationDate(1989);
+        System.out.println("book2.getPublicationDate() = " + book2.getPublicationDate());
     }
 }
